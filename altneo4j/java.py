6706d3970c9891b25b9282439a44a358fa8de37d
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2012-09-28 19:58:53 +0200

    jnius implementation

--- altneo4j/java.py
+++ altneo4j/java.py
@@ -1,33 +1,22 @@
 import os
 import sys
-import inspect
-from pkg_resources import resource_listdir
+
 from pkg_resources import resource_filename
 
-import jpype
+
+class_path = resource_filename(__name__, 'javalib')
 
 
+divider = ';' if sys.platform == "win32" else ':'
+os.environ['CLASSPATH'] = '%s%s%s/*' % (
+    os.environ['CLASSPATH'],
+    divider,
+    class_path
+)
 
 
-# build jvmargs
-jars = []
-for name in resource_listdir(__name__, 'javalib'):
-    if name.endswith('.jar'):
-        jars.append(resource_filename(__name__, "javalib/%s" % name))
-if len(jars) > 0:
-    divider = ';' if sys.platform == "win32" else ':'
-    classpath = divider.join(jars)
+from jnius import autoclass
 
-jvm_args = ['-Djava.class.path=%s' % classpath]
-
-if os.getenv('DEBUG', None) == 'true':
-    jvm_args.append('-Xdebug')
-    jvm_args.append('-Xnoagent')
-    jvm_args.append('-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000')
-
-
-# run jvm
-jpype.startJVM(jpype.getDefaultJVMPath(), *jvm_args)
 
 NEO4J_JAVA_CLASSES = (
     'org.neo4j.graphdb.Direction',
@@ -39,8 +28,6 @@ NEO4J_JAVA_CLASSES = (
 
 
 for class_path in NEO4J_JAVA_CLASSES:
-    class_path = class_path.split('.')
-    package, class_name = '.'.join(class_path[:-1]), class_path[-1]
-    package = jpype.JPackage(package)
-    klass = getattr(package, class_name)
+    class_name = class_path.split('.')[-1]
+    klass = autoclass(class_path)
     globals()[class_name] = klass
